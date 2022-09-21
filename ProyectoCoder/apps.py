cccc545cc07f59b1997e485cb0b49c4ece0dcc53
Salomon Oscar Atach <salomon_atach@yahoo.com.ar>
2022-09-21 16:54:28 -0300

    Segundo comentario

---
+++ ProyectoCoder/apps.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .model import *
+
+admin.site register (curso)
+admin.site register (estudiante)
+admin.site register (profesores)
+admin.site register (entregable)
