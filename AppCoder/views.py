fa3ee761ef0902f6668b260499b4c63cf458026a
Saba Miro <sabam1015@gmail.com>
2022-09-21 00:20:17 -0300

    creacion de primera vista y los modelos, uso de template basica de html con bootstrap

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.http.request import QueryDict
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from AppCoder.models import *
 # Create your views here.
+def index(request):
+
+      return render(request, 'padre.html')
